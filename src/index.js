84af018797f90d378b228cfecdd0cda3879d0188
Chris Sullivan <chrissullivan.dev@gmail.com>
2021-04-12 11:30:56 -0700

    Revert "fix: handle CloudFormation tags"

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const get = require('lodash.get');
 const glob = require('glob');
+const { resolve } = require('./utils/fs');
 
 const mergeTest = require('./utils/merge');
 
@@ -14,7 +15,6 @@ const {
 class Modularize {
   constructor(sls) {
     this.serverless = sls;
-    this.resolve = filename => this.serverless.utils.readFileSync(filename);
 
     this.hooks = {
       [`${PLUGIN}:info:info`]: this.printInfo.bind(this),
@@ -67,7 +67,7 @@ class Modularize {
 
   printInfo() {
     for (const file of this.files) {
-      this.log(file, '\n', JSON.stringify(this.resolve(file), null, 2), '\n');
+      this.log(file, '\n', JSON.stringify(resolve(file), null, 2), '\n');
     }
   }
 
@@ -80,7 +80,7 @@ class Modularize {
   mergeModules() {
     const subset = this.getSubset();
 
-    const mergedValues = [subset, ...this.files.map(this.resolve)].reduce(
+    const mergedValues = [subset, ...this.files.map(resolve)].reduce(
       mergeTest,
       {},
     );
@@ -95,7 +95,7 @@ class Modularize {
       const filename = match[0].slice(1, -1);
 
       Object.assign(this.serverless.service, {
-        custom: this.resolve(filename),
+        custom: resolve(filename),
       });
     }
   }
