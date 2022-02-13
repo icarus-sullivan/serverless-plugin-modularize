5987d29357fc8d1511b67995bae166bc246fb980
Chris Sullivan <chrissullivan.dev@gmail.com>
2022-02-12 20:46:14 -0800

    Adding update call to service, changing custom construction

--- src/config.js
+++ src/config.js
@@ -3,5 +3,4 @@ module.exports = {
   IDENTIFIER: 'modularize:',
   IDENTIFIER_COLOR: '\x1b[32m',
   COLOR_RESET: '\x1b[0m',
-  FILE_REGEX: /\(.+\)/g,
 };
--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
 const get = require('lodash.get');
 const glob = require('glob');
+const { clean } = require('@teleology/fp');
+// merge for local merge output, not for serverless
+const merge = require('lodash.merge');
 const { resolve } = require('./utils/fs');
-
-const mergeTest = require('./utils/merge');
 
 const {
   PLUGIN,
   IDENTIFIER_COLOR,
   IDENTIFIER,
   COLOR_RESET,
-  FILE_REGEX,
 } = require('./config');
 
 class Modularize {
   constructor(sls) {
     this.serverless = sls;
+
     this.resolve = (filename) => {
       try {
         return resolve(filename);
       } catch (e) {
         return this.serverless.utils.readFileSync(filename);
       }
-    }
+    };
 
     this.hooks = {
       [`${PLUGIN}:info:info`]: this.printInfo.bind(this),
       [`${PLUGIN}:merged:merged`]: this.printMerged.bind(this),
     };
-
     this.commands = {
       [PLUGIN]: {
         usage: 'Merging modularized serverless components',
@@ -45,7 +45,6 @@ class Modularize {
       },
     };
 
-    this.processCustom();
     const options = get(this.serverless, 'service.custom.modularize');
     if (!options || !options.glob) {
       this.files = [];
@@ -63,13 +62,13 @@ class Modularize {
   }
 
   getSubset() {
-    return {
+    return clean({
       plugins: get(this.serverless, 'service.plugins', []),
       custom: get(this.serverless, 'service.custom', {}),
       provider: get(this.serverless, 'service.provider', {}),
       functions: get(this.serverless, 'service.functions', {}),
       resources: get(this.serverless, 'service.resources', {}),
-    };
+    });
   }
 
   printInfo() {
@@ -79,31 +78,17 @@ class Modularize {
   }
 
   printMerged() {
-    const subset = this.getSubset();
-
+    const subset = this.files.reduce(
+      (c, file) => merge(c, this.resolve(file)),
+      this.getSubset(),
+    );
     this.log(JSON.stringify(subset, null, 2));
   }
 
   mergeModules() {
-    const subset = this.getSubset();
-
-    const mergedValues = [subset, ...this.files.map(this.resolve)].reduce(
-      mergeTest,
-      {},
-    );
-
-    Object.assign(this.serverless.service, mergedValues);
-  }
-
-  processCustom() {
-    const c = get(this.serverless, 'service.custom');
-    const match = FILE_REGEX.exec(c);
-    if (match && match[0]) {
-      const filename = match[0].slice(1, -1);
-
-      Object.assign(this.serverless.service, {
-        custom: this.resolve(filename),
-      });
+    for (const file of this.files) {
+      const content = this.resolve(file);
+      this.serverless.service.update(content);
     }
   }
 }
